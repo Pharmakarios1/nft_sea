a643fd4f5e23348919b130663e60318bec3aa9a0
pharmakarios1 <akhigbeblessed@gmail.com>
2025-06-14 17:08:02 +0100

    mobile links added

--- src/components/customs/howItworks/index.tsx
+++ src/components/customs/howItworks/index.tsx
@@ -1,15 +1,19 @@
 const HowItWorks = () => {
   return (
-    <div className="w-[90%] mx-auto mt-5">
-      <div className="container flex flex-col gap-5 p-10">
-        <h2 className="text-xl md:text-3xl text-center font-bold">
-          How
-          <span className="ml-2 text-transparent bg-clip-text bg-gradient-to-tr from-indigo-600 to-pink-500">
-            It Works
-          </span>
-        </h2>
-        <div className="grid grid-cols-1 md:grid-cols-2 mx-auto place-items-center gap-10 md:gap-5 w-full p-5 my-5 shadow-pink-400 shadow-lg rounded-md">
-          <img src="/metamask.png" alt="" className="w-full h-full" />
+    <div className="w-full md:w-[80%] mx-auto mt-5">
+      <h2 className="text-xl md:text-3xl text-center font-bold">
+        How
+        <span className="ml-2 text-transparent bg-clip-text bg-gradient-to-tr from-indigo-600 to-pink-500">
+          It Works
+        </span>
+      </h2>
+      <div className="flex flex-col gap-5 ">
+        <div className="grid grid-cols-1 md:grid-cols-2  place-items-center gap-10 md:gap-5 p-5 my-5 shadow-pink-400 shadow-lg rounded-md">
+          <img
+            src="/metamask.png"
+            alt=""
+            className="w-full h-full rounded-3xl"
+          />
           <div className="">
             <h1 className="md:text-start md:w-[200px] text-2xl">
               Set up and connect your wallet
@@ -27,7 +31,7 @@ const HowItWorks = () => {
             </p>
           </div>
         </div>
-        <div className="grid grid-cols-1 md:grid-cols-2 mx-auto gap-10 md:gap-5 w-full p-5  my-5 shadow-indigo-400 shadow-lg rounded-md">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-10 md:gap-5 p-5  my-5 shadow-indigo-400 shadow-lg rounded-md">
           <img
             src="/digital.jpg"
             alt=""
